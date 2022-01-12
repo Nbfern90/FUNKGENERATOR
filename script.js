c5cd9a1aecfa8fd6b1ba37df1ca947e79fb61945
Nick Fernandez <nbfern90@gmail.com>
2022-01-12 15:39:03 -0700

    motified shuffle function, added video

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-
+var video = document.getElementById("movie");
 
 var randomVid = [
   '/vids/Cisco Kid.mp4',
@@ -21,25 +20,45 @@ var randomVid = [
   '/vids/Lets Groove.mp4',
   '/vids/Slipping Into Darkness.mp4',
   '/vids/Back Stabbers.mp4',
-  '/vids/Couldnt Get It Right.mp4',       
+  '/vids/Couldnt Get It Right.mp4',
+  '/vids/Flashlight.mp4',       
   
-
 ];
-var video = document.getElementById("movie");
-
 
 function randomFunk() {
 var vidIndex = Math.floor(Math.random()*randomVid.length);  
 
 var shuffle = randomVid[vidIndex];
-
-
 video.src = shuffle;
-console.log(video.src);                  
+
+randomVid.splice(vidIndex,1)
+
+console.log(randomVid);
+
+if (randomVid.length === 0){
+  randomVid.push(
+    '/vids/Cisco Kid.mp4',
+  '/vids/Gap Band.mp4',
+  '/vids/Just Begun.mp4',
+  '/vids/Let it Whip.mp4',
+  '/vids/Move On Up.mp4',
+  '/vids/Parliment.mp4',
+  '/vids/Rock Steady.mp4',
+  '/vids/Superstition.mp4',
+  '/vids/Stretch.mp4',
+  '/vids/Do it right.mp4',
+  '/vids/Burn Rubber.mp4',
+  '/vids/Mary Jane.mp4',
+  '/vids/Get Down Tonight.mp4',
+  '/vids/Superfly.mp4',
+  '/vids/Got to Give it Up.mp4',
+  '/vids/The Payback.mp4',
+  '/vids/Lets Groove.mp4',
+  '/vids/Slipping Into Darkness.mp4',
+  '/vids/Back Stabbers.mp4',
+  '/vids/Couldnt Get It Right.mp4',
+  '/vids/Flashlight.mp4',       
+  )
 }
 
-
-
-
-
-
+}
