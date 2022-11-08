bd937c0fcca628fe20120bdb7ed9a48c6c66fe70
Nick Fernandez <nbfern90@gmail.com>
2022-11-08 15:38:55 -0600

    Reorganized files and added Media Queries

--- script.js
+++ script.js
@@ -1,74 +1,72 @@
 var video = document.getElementById("movie");
 
 var randomVid = [
-  '/vids/Cisco Kid.mp4',
-  '/vids/Gap Band.mp4',
-  '/vids/Just Begun.mp4',
-  '/vids/Let it Whip.mp4',
-  '/vids/Move On Up.mp4',
-  '/vids/Parliment.mp4',
-  '/vids/Rock Steady.mp4',
-  '/vids/Superstition.mp4',
-  '/vids/Stretch.mp4',
-  '/vids/Do it right.mp4',
-  '/vids/Burn Rubber.mp4',
-  '/vids/Mary Jane.mp4',
-  '/vids/Get Down Tonight.mp4',
-  '/vids/Superfly.mp4',
-  '/vids/Got to Give it Up.mp4',
-  '/vids/The Payback.mp4',
-  '/vids/Lets Groove.mp4',
-  '/vids/Slipping Into Darkness.mp4',
-  '/vids/Back Stabbers.mp4',
-  '/vids/Couldnt Get It Right.mp4',
-  '/vids/Flashlight.mp4',
-  '/vids/Hollywood Swinging.mp4',
-  '/vids/Live it up.mp4',
-  '/vids/Fantastic Voyage.mp4',
-  '/vids/Machine Gun.mp4',
-  '/vids/Get down on it.mp4',       
-  
+  "/vids/Cisco Kid.mp4",
+  "/vids/Gap Band.mp4",
+  "/vids/Just Begun.mp4",
+  "/vids/Let it Whip.mp4",
+  "/vids/Move On Up.mp4",
+  "/vids/Parliment.mp4",
+  "/vids/Rock Steady.mp4",
+  "/vids/Superstition.mp4",
+  "/vids/Stretch.mp4",
+  "/vids/Do it right.mp4",
+  "/vids/Burn Rubber.mp4",
+  "/vids/Mary Jane.mp4",
+  "/vids/Get Down Tonight.mp4",
+  "/vids/Superfly.mp4",
+  "/vids/Got to Give it Up.mp4",
+  "/vids/The Payback.mp4",
+  "/vids/Lets Groove.mp4",
+  "/vids/Slipping Into Darkness.mp4",
+  "/vids/Back Stabbers.mp4",
+  "/vids/Couldnt Get It Right.mp4",
+  "/vids/Flashlight.mp4",
+  "/vids/Hollywood Swinging.mp4",
+  "/vids/Live it up.mp4",
+  "/vids/Fantastic Voyage.mp4",
+  "/vids/Machine Gun.mp4",
+  "/vids/Get down on it.mp4",
 ];
 
 function randomFunk() {
-var vidIndex = Math.floor(Math.random()*randomVid.length);  
+  var vidIndex = Math.floor(Math.random() * randomVid.length);
 
-var shuffle = randomVid[vidIndex];
-video.src = shuffle;
+  var shuffle = randomVid[vidIndex];
+  video.src = shuffle;
 
-randomVid.splice(vidIndex,1)
+  randomVid.splice(vidIndex, 1);
 
-console.log(randomVid);
+  console.log(randomVid);
 
-if (randomVid.length === 0){
-  randomVid.push(
-    '/vids/Cisco Kid.mp4',
-  '/vids/Gap Band.mp4',
-  '/vids/Just Begun.mp4',
-  '/vids/Let it Whip.mp4',
-  '/vids/Move On Up.mp4',
-  '/vids/Parliment.mp4',
-  '/vids/Rock Steady.mp4',
-  '/vids/Superstition.mp4',
-  '/vids/Stretch.mp4',
-  '/vids/Do it right.mp4',
-  '/vids/Burn Rubber.mp4',
-  '/vids/Mary Jane.mp4',
-  '/vids/Get Down Tonight.mp4',
-  '/vids/Superfly.mp4',
-  '/vids/Got to Give it Up.mp4',
-  '/vids/The Payback.mp4',
-  '/vids/Lets Groove.mp4',
-  '/vids/Slipping Into Darkness.mp4',
-  '/vids/Back Stabbers.mp4',
-  '/vids/Couldnt Get It Right.mp4',
-  '/vids/Flashlight.mp4',
-  '/vids/Hollywood Swinging.mp4',
-  '/vids/Live it up.mp4',
-  '/vids/Fantastic Voyage.mp4',
-  '/vids/Machine Gun.mp4',
-  '/vids/Get down on it.mp4',       
-  )
-}
-
+  if (randomVid.length === 0) {
+    randomVid.push(
+      "/vids/Cisco Kid.mp4",
+      "/vids/Gap Band.mp4",
+      "/vids/Just Begun.mp4",
+      "/vids/Let it Whip.mp4",
+      "/vids/Move On Up.mp4",
+      "/vids/Parliment.mp4",
+      "/vids/Rock Steady.mp4",
+      "/vids/Superstition.mp4",
+      "/vids/Stretch.mp4",
+      "/vids/Do it right.mp4",
+      "/vids/Burn Rubber.mp4",
+      "/vids/Mary Jane.mp4",
+      "/vids/Get Down Tonight.mp4",
+      "/vids/Superfly.mp4",
+      "/vids/Got to Give it Up.mp4",
+      "/vids/The Payback.mp4",
+      "/vids/Lets Groove.mp4",
+      "/vids/Slipping Into Darkness.mp4",
+      "/vids/Back Stabbers.mp4",
+      "/vids/Couldnt Get It Right.mp4",
+      "/vids/Flashlight.mp4",
+      "/vids/Hollywood Swinging.mp4",
+      "/vids/Live it up.mp4",
+      "/vids/Fantastic Voyage.mp4",
+      "/vids/Machine Gun.mp4",
+      "/vids/Get down on it.mp4"
+    );
+  }
 }
